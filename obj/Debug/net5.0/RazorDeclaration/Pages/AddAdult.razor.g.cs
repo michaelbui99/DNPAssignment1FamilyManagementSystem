7271c3732d148a46a9979847ff7c2c54bce12a6d
Michael Bui <michaelbui99@hotmail.com>
2021-10-04 15:48:46 +0200

    Changed add adult to family view styling

--- obj/Debug/net5.0/RazorDeclaration/Pages/AddAdult.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/AddAdult.razor.g.cs
@@ -13,84 +13,84 @@ namespace DNPAssignment1FamilyManagementSystem.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\_Imports.razor"
+#line 1 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\_Imports.razor"
+#line 2 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\_Imports.razor"
+#line 3 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\_Imports.razor"
+#line 4 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\_Imports.razor"
+#line 5 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\_Imports.razor"
+#line 6 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\_Imports.razor"
+#line 7 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\_Imports.razor"
+#line 8 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\_Imports.razor"
+#line 9 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\_Imports.razor"
 using DNPAssignment1FamilyManagementSystem;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\_Imports.razor"
+#line 10 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\_Imports.razor"
 using DNPAssignment1FamilyManagementSystem.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\Pages\AddAdult.razor"
+#line 2 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\Pages\AddAdult.razor"
 using DNPAssignment1FamilyManagementSystem.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\Pages\AddAdult.razor"
+#line 3 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\Pages\AddAdult.razor"
 using Models;
 
 #line default
@@ -105,7 +105,7 @@ using Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 73 "C:\Users\Micha\Documents\Coding\WebDev\FamilyManagementSystem\Pages\AddAdult.razor"
+#line 107 "C:\Users\Shark\Documents\Coding\DotNet\DNP1\FamilyManagementSystem\Pages\AddAdult.razor"
        
 
     [Parameter]
@@ -116,7 +116,7 @@ using Models;
 
     private Adult _newAdult = new Adult() {JobTitle = new Job()};
 
-    private string _selectedSex; 
+    private string _selectedSex;
 
 
     [CascadingParameter]
@@ -135,7 +135,7 @@ using Models;
     private void AddAdultToFamily()
     {
         Family familyOfAdult = FamilyService.GetFamily(StreetName, HouseNumber);
-        _newAdult.Sex = _selectedSex; 
+        _newAdult.Sex = _selectedSex;
         FamilyService.AddAdultToFamily(familyOfAdult, _newAdult);
         NavigationManager.NavigateTo("/Families");
     }
