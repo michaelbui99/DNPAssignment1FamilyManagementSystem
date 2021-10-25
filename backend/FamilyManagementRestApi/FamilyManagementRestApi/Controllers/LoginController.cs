d385cefbb9af9920b868e9fa63e905b2589edf3f
Michael Bui <michaelbui99@hotmail.com>
2021-10-25 16:47:23 +0200

    Generated new controller and added Login dtos

---
+++ backend/FamilyManagementRestApi/FamilyManagementRestApi/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using FamilyManagementRestApi.DTOs;
+using FamilyManagementRestApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyManagementRestApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController
+    {
+        private IUsersRepository _usersRepository;
+
+        public LoginController(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<LoginResponseDto>> LoginUser([FromBody] LoginRequestDto loginRequestDto)
+        {
+            return null;
+        }
+        
+    }
+}
---
+++ backend/FamilyManagementRestApi/FamilyManagementRestApi/DTOs/LoginResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using FamilyManagementRestApi.Models;
+
+namespace FamilyManagementRestApi.DTOs
+{
+    public class LoginResponseDto
+    {
+        public User User { get; set; }
+        public IEnumerable<string> ErrorMessages { get; set; }
+    }
+}
